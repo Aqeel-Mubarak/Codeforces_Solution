4ce9ab56e1bc52392147d707e9398e0c5ef80379
Fawad Mughal <117888890+Fawad-Mug@users.noreply.github.com>
2023-10-11 05:33:44 -0700

    Delete 1851A directory

--- 1851A/Escalator_conversations.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())):
-    n, m, k, h = map(int, input().split())
-    x = list(map(int, input().split()))
-    ran = [i*k for i in range(1, m)]
-    count = 0
-    for i in x:
-        if abs(h-i) in ran:
-            count += 1
-    print(count)
