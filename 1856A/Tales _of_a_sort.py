0584c49878faf973b5d96bfa63f17c0b479cc993
Fawad Mughal <117888890+Fawad-Mug@users.noreply.github.com>
2023-10-11 05:39:51 -0700

    Delete 1856A directory

--- 1856A/Tales _of_a_sort.py
+++
@@ -1,9 +0,0 @@
-t=int(input())
-for _ in range(t):
-    curr=0
-    n=int(input())
-    ls=list(map(int,input().split()))
-    for i in range(n-1):
-        if ls[i]>ls[i+1]:
-            curr=max(curr,ls[i])
-    print(curr)
