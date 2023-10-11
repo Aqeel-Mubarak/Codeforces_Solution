ae7e6bc82036ed43ec29fe97c76c00ffe989fbe1
Fawad Mughal <117888890+Fawad-Mug@users.noreply.github.com>
2023-10-11 05:40:11 -0700

    Delete 1873A directory

--- 1873A/Short_sort.py
+++
@@ -1,6 +0,0 @@
-for _ in range(int(input())):
-    string = (input())
-    if string =="acb" or string =="abc" or string=="bac" or string=="cba":
-        print("YES")
-    else:
-        print('NO')
